fa45ec508ae8d5eed0a531a4c5a84b45db147b77
Valentin <valentinpuillandrepro@gmail.com>
2024-09-07 16:34:20 +0200

    add postgres

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Return the sum of x and y"""
+    return x + y
+
+
+def subtract(x, y):
+    """Return the difference of x and y"""
+    return y - x
